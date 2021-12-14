1fd50abba7d6fa73d601221d68a909f43e2b74f9
Oluwaniyi66 <oluwaniyiropo11@gmail.com>
2021-12-14 20:30:10 +0100

    porty service api

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::match(['post','get', ],'/welcome', [ServiceController::class, 'welcome']);
+Route::match(['post','get', ],'/services', [ServiceController::class, 'services']);
+
+
 
 Route::get('/', function () {
     return view('welcome');
